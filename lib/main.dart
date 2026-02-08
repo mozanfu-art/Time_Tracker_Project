b8f253f2d186de7b9ab9cea37ca02e43c182473e
mozanfu-art <mozanfu@gmail.com>
2026-02-08 12:24:42 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -21,24 +21,11 @@ class TimeTrackingApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: const Color(0xFF4A9084),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF4A9084),
-          foregroundColor: Colors.white,
-        ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Color(0xFFFFD54F),
-          foregroundColor: Colors.black,
-        ),
-        cardTheme: const CardThemeData(
-           elevation: 3,
-           margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-           shape: RoundedRectangleBorder(
-           borderRadius: BorderRadius.all(Radius.circular(12)),
-           ),
-         ),
-        ),
-           home: const HomeScreen(),
-           );
-         }
-      }
+        // Use Material 3 defaults and color scheme instead of CardTheme
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
